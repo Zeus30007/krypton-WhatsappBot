280c1cb26ab2b7d002ee4ee89b91758aa4e94378
Zeus30007 <130933523+Zeus30007@users.noreply.github.com>
2023-10-28 06:34:41 +0200

    Update getConfig.js

--- src/getConfig.js
+++ src/getConfig.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 module.exports.getConfig = () => {
     return {
-        name: process.env.NAME || 'Krypton',
-        prefix: process.env.PREFIX || '#',
+        name: process.env.NAME || 'Mabu',
+        prefix: process.env.PREFIX || '-',
         writesonicAPI: process.env.WRITE_SONIC || null,
         bgAPI: process.env.BG_API_KEY || null,
         mods: (process.env.MODS || '').split(',')
